5660d324a621a2ad75418801effb7145deb96aaa
Hedi Sabah <headesabah@gmail.com>
2018-09-05 16:04:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,28 +6,28 @@ function driversWithRevenueOver (drivers, revenue) {
   });
 }
 
-// function driverNamesWithRevenueOver (drivers, revenue) {
-//   return driversWithRevenueOver(drivers, revenue)
-//     .map(function (driver) {
-//       return driver.name;
-//     });
-// }
+function driverNamesWithRevenueOver (drivers, revenue) {
+  return driversWithRevenueOver(drivers, revenue)
+    .map(function (driver) {
+      return driver.name;
+    });
+}
 
-// function exactMatch (drivers, matcher) {
-//   return drivers.filter(function (driver) {
-//     let matches = false;
+function exactMatch (drivers, matcher) {
+  return drivers.filter(function (driver) {
+    let matches = false;
 
-//     for (const key in matcher) {
-//       matches = driver[key] === matcher[key];
-//     }
+    for (const key in matcher) {
+      matches = driver[key] === matcher[key];
+    }
 
-//     return matches;
-//   });
-// }
+    return matches;
+  });
+}
 
-// function exactMatchToList (drivers, matcher) {
-//   return exactMatch(drivers, matcher)
-//     .map(function (driver) {
-//       return driver.name;
-//     });
-// }
+function exactMatchToList (drivers, matcher) {
+  return exactMatch(drivers, matcher)
+    .map(function (driver) {
+      return driver.name;
+    });
+}
